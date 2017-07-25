049927ab5b6224fd974a97ca021e8a4539180024
Nithish Bharath <nithish.619@gmail.com>
2017-07-25 12:41:08 +0530

    first commit

---
+++ src/com/covariant/Test.java
@@ -0,0 +1,31 @@
+package com.covariant;
+
+class Book {
+
+	Object read() {
+		System.out.println("Can't read a book");
+		return null;
+	}
+}
+
+class ScientificBook extends Book {
+	Integer read() {
+		System.out.println("Can read a scientific book");
+		return null;
+	}
+
+}
+
+public class Test {
+	public static void main(String[] args) {
+		Book book = new Book();
+		book.read();
+
+		ScientificBook scientificBook = new ScientificBook();
+		scientificBook.read();
+
+		Book books = new ScientificBook();
+		books.read();
+	}
+
+}
