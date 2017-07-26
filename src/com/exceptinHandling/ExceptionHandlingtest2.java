e9851d1758d0b38f2e0408ee66ba78ff77e715bd
nithish1234 <nithish.619@gmail.com>
2017-07-26 18:06:36 +0530

    removed catch block

--- src/com/exceptinHandling/ExceptionHandlingtest2.java
+++ src/com/exceptinHandling/ExceptionHandlingtest2.java
@@ -1,16 +1,12 @@
 package com.exceptinHandling;
 
 public class ExceptionHandlingtest2 {
-public static void main(String[] args) {
+public static void main(String[] args) throws ArrayIndexOutOfBoundsException {
 	try {
 		int a[]=new int[5];
 		 a[10]=30;
 	}	
 	catch (Exception e) {
-		// TODO: handle exception
-		System.out.println(e);
-	}
-	catch (ArrayIndexOutOfBoundsException e) {
 		// TODO: handle exception
 		System.out.println(e);
 	}
